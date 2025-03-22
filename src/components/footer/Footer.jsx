ea48ad58a70efce89be07968825501d185a22c1a
DeepaBuchepalli <deepabuchepalli@gmail.com>
2025-03-22 12:31:19 +0530

    Update Footer.jsx

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,7 +4,7 @@ import './footer.css';
 const Footer = () => {
   return (
     <footer>
-      <a href="#home" className="footer__logo">Navyasri</a>
+      <a href="#home" className="footer__logo">Sai Deepa</a>
       <ul className="permalinks">
         <li><a href="#home">Home</a></li>
         <li><a href="#about">About</a></li>
